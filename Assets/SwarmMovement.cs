aaf9b8a764ed483305f5f90fae9c92391faf5491
Said Burhan <saidburhan@gmail.com>
2022-02-05 00:34:25 +0300

    Burhanin kutsal parmak ölçüsü

--- Assets/SwarmMovement.cs
+++ Assets/SwarmMovement.cs
@@ -6,13 +6,20 @@ public class SwarmMovement : MonoBehaviour
 {
     public Transform swarmTarget;
 	private float swarmSpeed = .05f;
+	bool isMovementEnable = true;
 	private void Update()
 	{
+		if(isMovementEnable)
 		transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z-swarmSpeed);
 	}
 
 	Vector3 CalculateSwarmTargetPosition()
 	{
 		return new Vector3(transform.position.x,transform.position.y,swarmTarget.position.z);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("area")) isMovementEnable = false;
 	}
 }
